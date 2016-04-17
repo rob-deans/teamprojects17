56b9a9f3af0a4fd73987ed69adcbaea7add49983
Ben why do you care <Ben why do you care>
2016-04-17 12:24:04 +0100

    Revert "This is only to let me pull"
    This reverts commit 3fb0852638d415d920dab57b618905fcc80a1c1d.

--- teamprojects17/teamprojects17/Controllers/HomeController.cs
+++ teamprojects17/teamprojects17/Controllers/HomeController.cs
@@ -3,9 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using System.Configuration;
-using System.Data;
-using System.Data.SqlClient;
 
 namespace teamprojects17.Controllers
 {
@@ -58,20 +55,5 @@ namespace teamprojects17.Controllers
 
             return View();
         }
-
-    }
-
-    protected void Page_Load(object sender, EventArgs e)
-    {
-        using (SqlConnection cn = new SqlConnection(ConfigurationManager.ConnectionStrings["DBcon"].ToString()))
-        {
-            SqlCommand cmd = new SqlCommand("SELECT * FROM Park", cn);
-            cn.Open();
-            DropDownList1.DataSource = cmd.ExecuteReader();
-            DropDownList1.DataTextField = "ParkName";
-            DropDownList1.DataValueField = "ParkID";
-            DropDownList1.DataBind();
-        }
-
     }
 }
