82c16f8c24ba6b600ce46eb01517a374dbe41c18
Rob Deans <robertdeans@ntlworld.com>
2016-04-27 10:54:31 +0100

    Added model and controller

---
+++ teamprojects17/teamprojects17/Models/TimetableModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace teamprojects17.Models
+{
+    public class TimetableModel : DbContext
+    {
+        public int ReqId { get; set; }
+        public string ModCode { get; set; }
+        public int Day { get; set; }
+        public int Period { get; set; }
+        public int Week { get; set; }
+        public string BuildingCode { get; set; }
+        public int ParkId { get; set; }
+        public int DetailsYear { get; set; }
+        public int DetailsSemester { get; set; }
+    }
+
+    public class DbCon : DbContext
+    {
+        public DbSet<TimetableModel> Timetable { get; set; }
+    }
+}
