27e92af477ab0076c8093a285e5acc3ce1b25acc
Rhiannon Hines <rhiannonhines@live.co.uk>
2016-05-07 06:16:01 +0100

    Dashboard functionality (I need coffee to complete it)

--- teamprojects17/teamprojects17/Controllers/HomeController.cs
+++ teamprojects17/teamprojects17/Controllers/HomeController.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data;
+using System.Text;
+using System.Configuration;
+using System.Data.SqlClient;
 
 namespace teamprojects17.Controllers
 {
